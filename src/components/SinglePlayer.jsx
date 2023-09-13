140e4cad10bd199a600337cccd92fc72c96ae86a
hosma001 <haroonosmanzada@gmail.com>
2023-09-13 19:39:03 -0400

    fixed data import of single player

--- src/components/SinglePlayer.jsx
+++ src/components/SinglePlayer.jsx
@@ -4,13 +4,12 @@ import axios from "axios";
 
 const SinglePlayer = () => {
     const {id} = useParams()
-    console.log(id)
     const [player, setPlayer] = useState(null);
 
     useEffect(() => {
         async function fetchPlayer() {
             const response = await axios.get(`https://fsa-puppy-bowl.herokuapp.com/api/2307-FTB-ET-WEB-FT/players/${id}`);
-            const data = response.data.data.players;
+            const data = response.data.data.player;
             setPlayer(data);
         }
         fetchPlayer();
